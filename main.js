d8a817d6a5a14ef434661a2b635f4a8aeb146fb9
Cheetack <31681741+ChoYangLue@users.noreply.github.com>
2019-04-16 20:55:52 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -39,12 +39,21 @@ function greet() {
 }
 
 let index = 1;
+let alt_key = false;
 function keyPressed() {
+    
+    if (keyIsDown(ALT)){
+        alt_key = true;
+    } else {
+        alt_key = false;
+    }
+    
     if (keyCode === UP_ARROW) {
         //value = 255;
         background('#d5cfb6');
         if (index > 1) index -= 1;
         fill(102); // 塗りつぶしの色
+        if (alt_key) fill('#ba684f'); 
         rect(input.x, 65+low_space*index, 200, 20); // 四角形を生成(左上のx軸, 左上のy軸, 横幅, 縦幅)
         
     } else  if (keyCode === DOWN_ARROW) {
@@ -52,6 +61,7 @@ function keyPressed() {
         background('#d5cfb6');
         index += 1;
         fill(102); // 塗りつぶしの色
+        if (alt_key) fill('#ba684f'); 
         rect(input.x, 65+low_space*index, 200, 20); // 四角形を生成(左上のx軸, 左上のy軸, 横幅, 縦幅)
         
     }
@@ -61,7 +71,10 @@ function keyPressed() {
         else if (task_list[index-1].status == "DONE") task_list[index-1].status = "TODO";
         task_list[index-1].name.html(task_list[index-1].status + ": " + task_list[index-1].n_text);
     }
+    
+
 }
+
 
 function TaskBox(name, sta, rank) {
     
